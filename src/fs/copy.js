6f955bf2ce0079fe91ffac73dff41f578b19a308
yourunb <bxyura@gmail.com>
2024-10-02 19:28:46 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
-const copy = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+import path from 'path';
 
-await copy();
+const folderPath = path.join(process.cwd(), 'src/fs/files');
+const newFolderPath = path.join(process.cwd(), 'src/fs/files_copy');
+
+const copy = async () => {
+  try {
+    await fs.promises.access(folderPath);
+    await fs.promises.access(newFolderPath);
+    console.error('FS operation failed');
+    return;
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.promises.mkdir(newFolderPath).catch((error) => {
+        console.error(error.message);
+      });
+    } else console.error(error);
+  }
+
+  const files = await fs.promises.readdir(folderPath);
+  await Promise.all(files.map(file => 
+    fs.promises.copyFile(path.join(folderPath, file), path.join(newFolderPath, file))
+  ));
+}
+
+await copy().catch((error) => {
+  console.error(error.message);
+});
