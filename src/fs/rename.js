aaa26ba2503d0d34d3cb3d746bad2972cff5961e
yourunb <bxyura@gmail.com>
2024-10-02 20:11:06 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
-const rename = async () => {
-    // Write your code here 
-};
+import path from 'path';
+import fs from 'fs';
 
-await rename();
+const filePath = path.join(process.cwd(), 'src/fs/files/wrongFilename.txt');
+const newFilePath = path.join(process.cwd(), 'src/fs/files/properFilename.md');
+
+const renameFile = async () => {
+  await await fs.promises.rename(filePath, newFilePath).catch(() => {
+    console.error('FS operation failed');
+  });
+}
+
+const rename = async () => {
+  try {
+    await fs.promises.access(filePath);
+    await fs.promises.access(newFilePath);
+    console.error('FS operation failed');
+  } catch (error) {
+      if (error.code === 'ENOENT') await renameFile();
+  }
+}
+
+await rename().catch((error) => {
+  console.error(error);
+});
