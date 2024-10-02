904394066d8743919da2cf67c4f45f4f8e4f033f
yourunb <bxyura@gmail.com>
2024-10-02 18:43:22 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+
+const folderPath = path.join(process.cwd(), 'src/fs/files');
+const filePath = path.join(folderPath, 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
-};
+  try {
+    await fs.promises.mkdir(folderPath, { recursive: true });
+    await fs.promises.access(filePath);
+    console.error('FS operation failed');
+  } catch (error) {
+    error.code === 'ENOENT' ? await fs.promises.writeFile(filePath, 'I am fresh and young') : console.error(error);
+  }
+}
 
 await create();
