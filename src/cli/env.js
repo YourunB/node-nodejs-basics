02e84b735482a5c32a7807731c62f8d35f20eb06
yourunb <bxyura@gmail.com>
2024-10-03 21:16:25 +0300

    feat: add function for check RSS_ variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = process.env;
+  const result = [];
+
+  for (const key in envVariables) {
+    if (key.startsWith('RSS_')) result.push(`${key}=${envVariables[key]}`);
+  }
+
+  const resultToString = result.join('; ');
+  console.log(resultToString);
 };
 
 parseEnv();
