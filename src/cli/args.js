5a090488aa478202f7e8353e29ec492016689b20
yourunb <bxyura@gmail.com>
2024-10-03 21:48:27 +0300

    feat: add function for analize args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const arg = process.argv;
+  const args = arg.slice(2);
+  const result = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const argName = args[i].replace('--', '');
+    const value = args[i + 1];
+    result.push(`${argName} is ${value}`);
+  }
+
+  const resultString = result.join(', ');
+  console.log(resultString); 
 };
 
 parseArgs();
