bd40bbb247064c680ffdc9f37e8c1cfa2fae6490
yourunb <bxyura@gmail.com>
2024-10-03 20:57:56 +0300

    feat: add complete function for read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const filePath = path.join(process.cwd(), 'src/fs/files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
-};
+  try {
+    const text = await fs.readFile(filePath, 'utf-8');
+    console.log(text);
+  } catch {
+    console.error('Error: FS operation failed');
+  }
+}
 
 await read();
