eaf0cf8d0f287e5452b544b1faafaab97a3113c2
yourunb <bxyura@gmail.com>
2024-10-04 20:30:28 +0300

    feat: add complete calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import path from 'path';
+
+const filePath = path.join('src/hash/files/fileToCalculateHashFor.txt');
+const pipelineRead = promisify(pipeline);
+
 const calculateHash = async () => {
-    // Write your code here 
-};
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  try {
+    await pipelineRead(stream, hash);
+    console.log(hash.digest('hex'));
+  } catch {
+      console.error('Pipeline failed');
+  }
+}
 
 await calculateHash();
