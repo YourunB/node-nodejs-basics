0fe2265ae81cef6304c7fa0b534ff716a32c81d1
yourunb <bxyura@gmail.com>
2024-10-06 22:00:47 +0300

    feat: add complete write stream function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import fs from 'fs';
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(process.cwd(), 'src/streams/files/fileToWrite.txt');
+  const write = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(write);
+
+  write.on('finish', () => console.log('Success'));
+  write.on('error', () => console.error('Error to write'));
+  console.log('Enter text:');
 };
 
 await write();
