84b3b90e5b72cd11961336ac5fd5b000aaa53162
yourunb <bxyura@gmail.com>
2024-10-06 23:27:54 +0300

    feat: add main for work wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { cpus } from 'node:os';
+import { Worker } from 'node:worker_threads';
+import path from 'path';
+import * as url from 'url';
+
+const pathFolder = url.fileURLToPath(new URL('.', import.meta.url));
+const pithFile = path.resolve(pathFolder, './worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
-};
+  const cores = cpus().length;
+  const count = 10;
+  const status = {
+    success: 'resolved',
+    failure: 'error',
+  };
+
+  const promises = [];
+
+  for (let i = 0; i < cores; i += 1) {
+    promises.push(
+      new Promise((resolve) => {
+        const worker = new Worker(pithFile, { workerData: count + i });
+
+        worker.on('error', () => { resolve({ status: status.failure, data: null })});
+        worker.on('message', (data) => { resolve({ status: status.success, data })});
+      })
+    );
+  }
+
+  console.log(await Promise.all(promises));
+}
 
 await performCalculations();
